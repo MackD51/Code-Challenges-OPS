98b67b841bebbe3d2d2c8feb6d3382434b63e415
Mack Dirks <130383634+MackD51@users.noreply.github.com>
2023-04-27 11:28:30 -0400

    Add files via upload

---
+++ history.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Script Name:                          Login History       
+# Author:                               Mack Dirks                       
+# Date of latest revision:              04/26/2023     
+# Purpose:                              Practice writing functions and scripts
+
+# Declaration of variables: 
+
+# Declaration of functions:
+
+my_history()
+{
+    last
+}
+
+# Main
+
+#function call - 3 times:
+
+my_history
+my_history
+my_history
+
+echo "This is the login history."
+
+
+# End
