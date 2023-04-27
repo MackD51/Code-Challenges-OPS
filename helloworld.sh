785f8ea79435f10cd92eb72f118f64296d2db544
Mack.D <mack.a.dirks@gmail.com>
2023-04-27 15:31:05 +0000

    Add history.sh

---
+++ helloworld.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script Name:     helloworld.sh        
+# Author: Mack Dirks                       
+# Date of latest revision: 04/25/2023     
+# Purpose:  To practice a simple script
+
+# Declaration of variables: 
+# variable sentence was declared and initialized to "i am working perfectly"
+
+# Declaration of functions: none
+
+
+# Main
+
+echo "Hello, World!"
+
+sentence="I am working perfectly!"
+echo $sentence 
+
+# End
