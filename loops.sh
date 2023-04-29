d1fb7b919c74662143c2378f88c29f4c87c05981
Mack Dirks <130383634+MackD51@users.noreply.github.com>
2023-04-29 02:56:17 -0400

    Add files via upload

---
+++ loops.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                  Loop Practice
+# Author:                       Mack Dirks
+# Date of latest revision:      04/28/2023
+# Purpose:                      Practice loops
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+ps aux
+
+# End
