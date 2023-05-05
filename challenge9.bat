ec2a02c91bfa9b66384121304bfd7dcd6c3ada1c
MackD51 <mack.a.dirks@gmail.com>
2023-05-05 06:46:44 +0000

    Challenege9 Commit

---
+++ challenge9.bat
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script Name:                  Challenge 9
+# Author:                       Mack Dirks
+# Date of latest revision:      05/04/2023
+# Purpose:                      Log Retrieval via Powershell
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+
+
+
+# End
