de523549bfec2b840b678c5c9ab10067dbdc3a8e
MackD51 <mack.a.dirks@gmail.com>
2023-05-06 06:48:01 +0000

    Challenge 10 Commit

---
+++ challenge10.ps1
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script Name:                  Challenge 10
+# Author:                       Mack Dirks
+# Date of latest revision:      05/05/2023
+# Purpose:                      System Process Commands
+
+# Declaration of variables
+
+# Declaration of functions
+
+# Main
+
+
+# End
+
