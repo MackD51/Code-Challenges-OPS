882581f3de3f3805e48af314d12c37e84bc63579
MackD51 <mack.a.dirks@gmail.com>
2023-05-08 01:59:27 +0000

    Updated Version

--- challenge7.sh
+++ challenge7.sh
@@ -2,7 +2,7 @@
 
 # Script Name:                  Challenge 7
 # Author:                       Mack Dirks
-# Date of latest revision:      05/02/2023
+# Date of latest revision:      05/05/2023
 # Purpose:                      System Information
 
 # Declaration of variables
@@ -12,24 +12,25 @@
 
 # Main
 
-echo Name of the computer:
+echo NAME OF THE COMPUTER:
 sudo hostname
 
 echo "*******************************************************************************************"
 echo CPU Info:
-sudo lshw -short | grep -E 'processor|product|vendor|physical id|bus info|width'
+sudo lshw | grep *-cpu -A 6 | grep -v "version"
 
 echo "*******************************************************************************************"
 echo RAM Info:
-sudo lshw -short | grep -E 'memory|description|physical id|size'
+sudo lshw | grep *-memory -A 3
 
 echo "*******************************************************************************************"
 echo Adapter Info:
-sudo lshw -short | grep -E 'display|description|product|vendor|physical id|bus info|width|clock|capabilities|configuration|resources'
+sudo lshw | grep *-display -A 12 | head -6 
+sudo lshw | grep *-display -A 12 | tail -5
 
 echo "*******************************************************************************************"
 echo Network Adapter Info:
-sudo lshw -short | grep -E 'network|description|product|vendor|physical id|bus info|logical name|version|serial|size|capacity|width|clock|capabilities|configuration|resources'
+sudo lshw | grep *-network -A 15
 
 
 # End
