b61cd7703d3d71586884e27f2e0723036ede1a28
MackD51 <mack.a.dirks@gmail.com>
2023-05-11 10:53:59 +0000

    Challenge 13 commit

--- challenge13.sh
+++ challenge13.sh
@@ -7,22 +7,28 @@
 
 # Declaration of variables
 
+file="domain_information.txt"
+line_separator="****************************************************************"
+
 # Declaration of functions
 
-
-# Main
-
-file="domain_information.txt"
-user_input
+user_input()
 {
     echo "Enter a domain name (ex. google.com): "
     read domain_name
-    whois $domain_name
-    dig $domain_name
-    host $domain_name
-    nslookup $domain_name
+    {
+        whois $domain_name
+        echo "$line_separator"
+        dig $domain_name
+        echo "$line_separator"
+        host $domain_name
+        echo "$line_separator"
+        nslookup $domain_name
+    } >> $file
 }
 
+# Main
 
+user_input
 
 # End
