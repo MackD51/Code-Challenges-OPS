8ad80112c667ceea759d64c43874e820af045bbc
MackD51 <mack.a.dirks@gmail.com>
2023-06-02 15:47:04 +0000

    Challenge 3 commit

--- chall3.sh
+++ chall3.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# Script Name:                  Challenge 2
+# Script Name:                  Challenge 3
 # Author:                       Mack Dirks
-# Date of latest revision:      05/31/2023
-# Purpose:                      Append; Date and Time
-
+# Date of latest revision:      06/01/2023
+# Purpose:                      File Permissions
 
 
 # Main
+read -p "Provide the directory path: " dir_path
+
+perm=read "Provide the permission number: "
+
+cd "$dir_path"
+
+chmod "$perm" *
+
+ls -l
+
+stat -c
 
 
 
