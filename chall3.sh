3af2b81d5f42584a83c431929816971c8abdd1de
MackD51 <mack.a.dirks@gmail.com>
2023-06-02 15:38:21 +0000

    Challenge 3 commit

---
+++ chall3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script Name:                  Challenge 2
+# Author:                       Mack Dirks
+# Date of latest revision:      05/31/2023
+# Purpose:                      Append; Date and Time
+
+
+
+# Main
+
+
+
+# End
