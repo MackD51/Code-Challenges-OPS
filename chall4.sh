365e608ebe81e37fc0db150add808f7414b4ca7a
MackD51 <mack.a.dirks@gmail.com>
2023-06-03 11:42:43 +0000

    Challenge 4 commit

---
+++ chall4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script Name:                  Challenge 4
+# Author:                       Mack Dirks
+# Date of latest revision:      06/02/2023
+# Purpose:                      Conditionals in Menu Systems
+
+
+# Main
+
+
+
+
+# End
