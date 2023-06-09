b591665cb1f2d48ac09765e51790399f7e7144dc
MackD51 <mack.a.dirks@gmail.com>
2023-06-09 15:09:23 +0000

    Challenge 8 commit

---
+++ chall8.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Script Name:                  Challenge 8
+# Author:                       Mack Dirks
+# Date of latest revision:      06/08/2023
+# Purpose:                      Python Collections
+
+
+# Main
+
+list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print("\nThe fourth element of the list is: ", list[3])
+print("\nThe sixth through tenth element of the list: ", list[5:])
+list[6] = "onion"
+print("\nThe seventh element is:", list[6], "\n")
+
+
+# End
