b2affbcbb057571117b6abf1214680cbc5dc2581
MackD51 <mack.a.dirks@gmail.com>
2023-07-19 20:44:39 +0000

    update

--- 401_ch06.py
+++ 401_ch06.py
@@ -7,30 +7,95 @@
 
 # Imports cryptographic library
 from cryptography.fernet import Fernet
+from os.path import exists
+
+# Variable declaration
+answer = "yes"
+
+# Function to generate the key
+def gen_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as k_file:
+        k_file.write(key)
+
+# Function to load our key:
+def key_load():
+    return open("key.key", "rb").read()
+
 
 # File encryption function
-def file_encryption()
-    key = Fernet.generate_key()
+def file_encryption():
     f = Fernet(key)
+    file_location = input("\nEnter the file path: ")
+    with open(file_location, "rb") as file:
+        data = file.read()
+    encrypt_file = f.encrypt(data)
+    with open(file_location, "wb") as file:
+        file.write(encrypt_file)
 
 # File decryption function
-def file_decryptio()
-    key = Fernet.generate_key()
+def file_decryption():
     f = Fernet(key)
+    file_location = input("\nEnter the file path: ")
+    with open(file_location, "rb") as file:
+        data = file.read()
+        decrypt_file = f.decrypt(data)
+    with open(file_location, "wb") as file:
+        file.write(decrypt_file)
 
 # Message encryption function
-def message_encryption()
-    key = Fernet.generate_key()
+def message_encryption():
+    message = input("\nEnter a message to encrypt: ")
+    encrypt_message = message.encode()
     f = Fernet(key)
+    encrypt_version = f.encrypt(encrypt_message)
+    print("\nHere is the encrypted message: ")
+    print(encrypt_version)
 
 # Message decryption function
-def message_decryption()
-    key = Fernet.generate_key()
+def message_decryption():
+    message = input("\nEnter a message to decrypt: ")
+    decrypt_message = str.encode(message)
     f = Fernet(key)
+    decrypt_version = f.decrypt(decrypt_message)
+    print("\nHere is the encrypted message: ")
+    print(decrypt_version)
 
-# Creates a menu with 4 different options
-print("Here are your 4 options: \n")
-print("To Encrypt a File, select 1\n")
-print("To Decrypt a File, select 2\n")
-print("To Encrypt a Message, select 3\n")
-print("To Decrypt a Message, select 4\n")
+# Menu function
+def menu():
+    print("\nHere are your 4 options: ")
+    print("\nTo Encrypt a File, select 1")
+    print("To Decrypt a File, select 2")
+    print("To Encrypt a Message, select 3")
+    print("To Decrypt a Message, select 4")
+    user_choice = input("\nSelect your option: ")
+
+    if (user_choice == "1"):
+        file_encryption()
+        print("\nThe file was encrypted")
+    elif (user_choice == "2"):
+        file_decryption()
+        print("\nThe message was decrypted")
+    elif (user_choice == "3"):
+        message_encryption()
+    elif(user_choice == "4"):
+        message_decryption()
+    else:
+        print("Inappropriate selection!")
+
+# Main
+
+key_exists = exists("./key.key")
+
+if key_exists:
+    key = key_load()
+else:
+    gen_key()
+    key = key_load()
+
+while True:
+    menu()
+    answer = input("\nDo you want to try again? yes or no: ")
+    if answer == "no":
+        print("\nEnjoy your day!")
+        break
--- 401_ch07.py
+++ 401_ch07.py
@@ -6,31 +6,40 @@
 # Purpose:                      File Encryption Script Part 2 of 3
 
 import os
-import cryptography.fernet as fernet
+from cryptography.fernet import Fernet
 
+# Function to generate the key
+def gen_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as k_file:
+        k_file.write(key)
 
+# Function to load our key:
+def key_load():
+    return open("key.key", "rb").read
 
-def folder_encryption(folder_location, encryption_key)
-    for root, dirs, files in os.walk(folder_location):
+def folder_encryption(dir_location, encryption_key):
+    for root, dirs, files in os.walk(dir_location):
         for file_name in files:
             full_path = os.path.join(root, file_name)
             folder_encryption(full_path, encryption_key)
     
-def folder_decryption(folder_location, encryption_key)
-    for root, dirs, files in os.walk(folder_location):
+def folder_decryption(dir_location, encryption_key):
+    for root, dirs, files in os.walk(dir_location):
         for file_name in files:
             full_path = os.path.join(root, file_name)
-            folder_encryption(full_path, encryption_key)
+            folder_decryption(full_path, encryption_key)
 
 # Main
 print("To encrypt a folder, select 1")
 print("To decrypt a folder, select 2")
 user_choice = input("Make a selection now: ")
-folder_location = input("Provide the path to your folder: ")
+dir_location = input("Provide the path to your folder: ")
 encryption_key = fernet.Fernet.generate_key()
 
 if user_choice == "1":
-    folder_encryption(folder_location, encryption_key)
+    folder_encryption(dir_location, encryption_key)
 elif user_choice == "2":
-    folder_decryption(folder_location, encryption_key)
+    folder_decryption(dir_location, encryption_key)
 else:
+       print("Inappropriate selection!")
